f23742db814977ee361049a7fc5bb1567df2f86a
鈴木新之介 <shin.suzuki0605@gmail.com>
2025-03-25 23:53:26 +0900

    DB設定 認証機能実装

---
+++ app/Models/Message.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    // use HasFactory;
+
+    // フィラブルプロパティの追加
+    protected $fillable = [
+        'thread_id',
+        'message_en',
+        'message_ja',
+        'sender',
+        'audio_file_path'
+    ];
+
+    // threadメソッドを追加
+    public function thread()
+    {
+        return $this->belongsTo(Thread::class);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,12 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => 'password123',
+        ]);
+
+        $this->call([
+            ThreadSeeder::class,
+            MessageSeeder::class,
         ]);
     }
 }
